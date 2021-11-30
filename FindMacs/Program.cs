e1eb18ea5a54a67fc7684855f3e89afe3b8314d9
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-11-30 23:11:20 +0300

    Add project files.

---
+++ FindMacs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace FindMacs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var machines = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(m => m.OperationalStatus == OperationalStatus.Up);
+            foreach(var machine in machines)
+            {
+                var IP = machine.GetIPProperties().UnicastAddresses.Last().Address;
+                if(IP.GetAddressBytes().First() == 192)
+                {
+                    Console.WriteLine($"IP - {IP}");
+                    Console.WriteLine($"Family - {IP.AddressFamily}");
+                    Console.WriteLine($"Byte1 = {IP.GetAddressBytes().First()}");
+                    Console.WriteLine($"MAC - {machine.GetPhysicalAddress().GetAddressBytes().First()}");
+                    Console.WriteLine($"MAC - {machine.GetPhysicalAddress()}");
+                    Console.WriteLine("--------------------");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
