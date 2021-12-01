2be3af8bf021c6a95f77e80c407ef08eaed29b1c
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-12-01 16:26:14 +0300

    finish : finish app

--- FindMacs/Program.cs
+++ FindMacs/Program.cs
@@ -1,29 +1,101 @@
-﻿using System;
+﻿using DreamNucleus.Heos;
+using DreamNucleus.Heos.Commands.Player;
+using DreamNucleus.Heos.Infrastructure.Heos;
+using DreamNucleus.Heos.Infrastructure.Telnet;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FindMacs
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var machines = NetworkInterface.GetAllNetworkInterfaces()
-                .Where(m => m.OperationalStatus == OperationalStatus.Up);
-            foreach(var machine in machines)
+
+            List<MacIpPair> mip = new();
+            List<MacIpPair> denons = new();
+
+            System.Diagnostics.Process pProcess = new();
+            pProcess.StartInfo.FileName = "arp";
+            pProcess.StartInfo.Arguments = "-a ";
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.Start();
+            string cmdOutput = pProcess.StandardOutput.ReadToEnd();
+            string pattern = @"(?<ip>([0-9]{1,3}\.?){4})\s*(?<mac>([a-f0-9]{2}-?){6})";
+
+            foreach (Match m in Regex.Matches(cmdOutput, pattern, RegexOptions.IgnoreCase))
             {
-                var IP = machine.GetIPProperties().UnicastAddresses.Last().Address;
-                if(IP.GetAddressBytes().First() == 192)
+                mip.Add(new MacIpPair()
                 {
-                    Console.WriteLine($"IP - {IP}");
-                    Console.WriteLine($"Family - {IP.AddressFamily}");
-                    Console.WriteLine($"Byte1 = {IP.GetAddressBytes().First()}");
-                    Console.WriteLine($"MAC - {machine.GetPhysicalAddress().GetAddressBytes().First()}");
-                    Console.WriteLine($"MAC - {machine.GetPhysicalAddress()}");
-                    Console.WriteLine("--------------------");
+                    MacAddress = m.Groups["mac"].Value,
+                    IpAddress = m.Groups["ip"].Value
+                });
+            }
+
+            foreach(var machine in mip)
+            {
+                Console.WriteLine($"IP - {machine.IpAddress} --> MAC - {machine.MacAddress}");
+                var mac = machine.MacAddress.Split(new char[] { '-' });
+                if(mac[0] == "00" & mac[1] == "05" & mac[2] == "cd")
+                {
+                    denons.Add(machine);
                 }
             }
+            Console.WriteLine("-----------------------");
+            foreach(var denon in denons)
+            {
+                Console.WriteLine($"Denon - {denon.IpAddress} --> {denon.MacAddress}");
+                Ping ping = new();
+                IPAddress ip = IPAddress.Parse(denon.IpAddress);
+                PingReply reply = ping.Send(ip);
+                if(reply.Status == IPStatus.Success)
+                {
+                    try
+                    {
+                        var telnetClient = new SimpleTelnetClient(denon.IpAddress);
+                        var heosClient = new HeosClient(telnetClient, CancellationToken.None);
+                        var commandProcessor = new CommandProcessor(heosClient);
+                        var plyrs = await commandProcessor.Execute(new GetPlayersCommand());
+                        foreach (var p in plyrs.Payload)
+                        {
+                            Console.WriteLine($"Player ID - {p.Pid}");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Unable to establish TelNet connection to {ip}");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"{denon.IpAddress} --> Status : {reply.Status}");
+                }
+            }
+
+
+
+
+
+
+
+
             Console.ReadKey();
         }
+    }
+
+
+    public struct MacIpPair
+    {
+        public string MacAddress;
+        public string IpAddress;
     }
 }
